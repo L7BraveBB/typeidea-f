37f11c969d8c52c4a0698b2266c4bb10fe052b19
lu <baba@ludeMacBook-Pro.local>
2022-07-09 18:26:50 +0800

    1.抽调基类class base view和增加前端bootstrap框架并新增css样式和部分js

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,11 +1,78 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import DetailView, ListView
 
 from .models import Tag, Post, Category
 from config.models import SideBar
 
 
 # Create your views here.
+
+
+class CommonViewMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'sidebars': SideBar.get_all(),
+        })
+        context.update(Category.get_navs())
+        return context
+
+
+class IndexView(ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 1  # 自带分野共，此处，把每页的数量设置为5
+    context_object_name = 'post_list'  # 如果不设置此项，在模板中则需要使用object_list变量
+    template_name = 'blog/list.html'
+
+
+class CategoryView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({
+            'category': category,
+        })
+        return context
+
+    def get_queryset(self):
+        """重写queryset，根据分类过滤"""
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({
+            'tag': tag,
+        })
+        return context
+
+    def get_queryset(self):
+        """重写queryset，根据分类过滤"""
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag_id=tag_id)
+
+
+class PostDetailView(CommonViewMixin, DetailView):
+    queryset = Post.latest_posts()
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
+
+
+class PostListView(ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 1  # 自带分页功能，此处，把每页的数量设置为1
+    context_object_name = 'post_list'  # 如果不设置此项，在模板中则需要使用object_list变量
+    template_name = 'blog/list.html'
 
 
 def post_list(request, category_id=None, tag_id=None):  # 参数是从urls.py的url中传递过来的
@@ -43,6 +110,3 @@ def post_detail(request, post_id):
     }
     context.update(Category.get_navs())
     return render(request, 'blog/detail.html', context=context)
-
-
-
