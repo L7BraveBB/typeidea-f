51baf33b00b68dcabfe396a867445aa786d5103e
lu <baba@ludeMacBook-Pro.local>
2022-07-21 17:25:43 +0800

    ”1.增加评论模块，但是存在问题：文章详情没有评论区域。2.解决无法打开标签页，详见blog/view中TODO“

--- typeidea/config/views.py
+++ typeidea/config/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from django.shortcuts import redirect
+
+from .models import Link
+from blog.views import CommonViewMixin
 
 
 # Create your views here.
+
+
+class LinksListView(CommonViewMixin, ListView):
+    queryset = Link.objects.filter(status=Link.STATUS_NORMAL)
+    template_name = 'config/links.html'
+    context_object_name = 'links_list'
 
 
 def links(request):
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -16,20 +16,21 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from comment.views import CommentView
 from typeidea.custom_site import custom_site
-# from blog.views import post_list, post_detail, PostDetailView
 from blog.views import (IndexView, CategoryView, TagView, PostDetailView,
                         SearchView, AuthorView)
-from config.views import links
+from config.views import LinksListView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
     url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
-    url(r'^post/(?P<pk>\d+).html$', PostDetailView.as_view(), name='post-detail'),
-    url(r'^links/$', links, name='links'),
+    url(r'^post/(?P<pk>\d+).html/$', PostDetailView.as_view(), name='post-detail'),
+    url(r'^links/$', LinksListView.as_view(), name='links'),
     url(r'^search/$', SearchView.as_view(), name='search'),
     url(r'^author/(?P<owner_id>\d+)/$', AuthorView.as_view(), name='author'),
+    url(r'^comment/$', CommentView.as_view(), name='comment'),
 
     url(r'^super_admin/', admin.site.urls, name='super-admin'),
     url(r'^admin/', custom_site.urls, name='admin'),
